7a4641a1991fd6b09f2eca366e1cdb246fafe046
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-04-21 18:28:40 +0100

    Add working Dogs API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const dogsContainer = document.querySelector(".container");
+const button = document.querySelector(".btn");
+
+const renderDog = function (src) {
+  const html = `
+            <div class="content">
+                <img class="image" src="${src}" />
+            </div>
+            `;
+
+  dogsContainer.insertAdjacentHTML("beforeend", html);
+};
+
+const loadMoreDogs = function () {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => renderDog(data.message));
+};
+
+button.addEventListener("click", loadMoreDogs);
