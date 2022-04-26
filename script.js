b417e84c8a989802eb54678532514355b48345f4
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-04-26 12:35:46 +0100

    Change loadMoreDog function. Add async / await

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ const renderDog = function (src) {
   dogsContainer.insertAdjacentHTML("beforeend", html);
 };
 
-const loadMoreDogs = function () {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then((response) => response.json())
-    .then((data) => renderDog(data.message));
+// const loadMoreDogs = function () {
+//   fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((response) => response.json())
+//     .then((data) => renderDog(data.message));
+// };
+
+const loadMoreDogs = async function () {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await res.json();
+  renderDog(data.message);
 };
 
 button.addEventListener("click", loadMoreDogs);
