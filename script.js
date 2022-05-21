0ada616ac533d1f715edf853f2e64c11f1ff193d
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-05-21 12:57:41 +0100

    Add animals buttons

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
-const carts = document.getElementsByClassName("content");
+const initialImage = document.querySelector(".image");
 const loading = document.querySelector(".lds-dual-ring");
 const dogsContainer = document.querySelector(".container");
-const button = document.querySelector(".btn");
-const dog = document.querySelector("#dog");
+const button = document.querySelector(".btn--load");
+const dogBark = document.querySelector("#dog");
+
+const dogAndCatButtons = document.querySelectorAll(".btn-animal");
+
+// API
+const dogAPI = "https://dog.ceo/api/breeds/image/random";
+const catAPI = "https://cataas.com/cat?json=true";
+const catImage = "https://cataas.com";
+
+// Functions
+
+const changeImage = function (e) {
+  const sorce = e.target.dataset.src;
+  initialImage.src = sorce;
+};
 
 const renderDog = function (src) {
   const html = `
@@ -16,12 +30,6 @@ const renderDog = function (src) {
   dogsContainer.insertAdjacentHTML("beforeend", html);
 };
 
-// const loadMoreDogs = function () {
-//   fetch("https://dog.ceo/api/breeds/image/random")
-//     .then((response) => response.json())
-//     .then((data) => renderDog(data.message));
-// };
-
 const showHide = function (...elements) {
   console.log(elements);
   elements.forEach((el) => el.classList.toggle("hidden"));
@@ -31,18 +39,31 @@ const loadMoreDogs = async function () {
   try {
     showHide(loading, button);
 
-    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const res = await fetch(dogAPI);
     const data = await res.json();
     renderDog(data.message);
 
     showHide(loading, button);
 
     // dog barking
-    dog.currentTime = 0;
-    dog.play();
+    dogBark.currentTime = 0;
+    dogBark.play();
   } catch (err) {
     alert(err);
   }
 };
 
 button.addEventListener("click", loadMoreDogs);
+
+dogAndCatButtons.forEach((button) => {
+  button.addEventListener("click", changeImage);
+});
+// const loadCat = (async function () {
+//   try {
+//     const res = await fetch(catAPI);
+//     const data = await res.json();
+//     console.log(data.url);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
